1576d0450f8cc2a07e6a0e24ab6f561bf9456148
Nil <gamersclub3232@gmail.com>
2025-05-07 12:21:33 +0000

    Increased social media icon size and added social links

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning><body // Removed whitespace between html and body
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet" />
+      </head>
+      <body // Removed whitespace between html and body
         className={cn(
           'min-h-screen bg-background font-sans antialiased',
           inter.variable
@@ -30,6 +34,11 @@ export default function RootLayout({
       >
         {children}
         <Toaster /> {/* Add Toaster */}
+        <div className="social-icons" style={{ textAlign: 'center', marginBottom: '20px', fontSize: '25px' }}> {/* Added a div for centering and spacing */}
+          <a href="https://www.instagram.com/_s.d.nil_?igsh=YzljYTk1ODg3Zg==" target="_blank" className="footer-social" style={{ margin: '0 10px' }}><i className="bx bxl-instagram" style={{ fontSize: '25px' }}></i></a> {/* Added margin for spacing */}
+          <a href="https://www.facebook.com/share/15ZjjcF7VE/" target="_blank" className="footer-social" style={{ margin: '0 10px' }}><i className="bx bxl-facebook" style={{ fontSize: '25px' }}></i></a> {/* Added margin for spacing */}
+          <a href="https://github.com/S-D-Nil" target="_blank" className="about-social-icon" style={{ margin: '0 10px' }}><i className="bx bxl-github" style={{ fontSize: '25px' }}></i></a> {/* Added margin for spacing */}
+        </div>
       </body>
     </html>
   );
